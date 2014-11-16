1f49c810d3aeda394507dbc7876f34d98ea184bc
swordXfish <dimitry.komarov@gmail.com>
2014-11-16 23:42:03 +0200

    refactored structure

--- app/protected/config/main.php
+++ app/protected/config/main.php
@@ -7,7 +7,12 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Commend For Me',
+	'name'=>'Doski',
+    'theme'=>'bootstrap',
+
+    'aliases' => array(
+        'bootstrap' => 'ext.bootstrap',
+    ),
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -16,36 +21,46 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+        'bootstrap.behaviors.*',
+        'bootstrap.helpers.*',
+        'bootstrap.widgets.*'
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+        'bootstrap' => array(
+            'class'=>'bootstrap.BootStrapModule'
+        ),
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'1234',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+            'generatorPaths' => array('bootstrap.gii'),
 		),
-		*/
+
 	),
 
 	// application components
 	'components'=>array(
+        'bootstrap' => array(
+            'class' => 'bootstrap.components.BsApi'
+        ),
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-                'wall' => 'wall/view'
+        'urlManager'=>array(
+            'showScriptName' => false,
+            'urlFormat'=>'path',
+            'rules'=>array(
+                'wall' => 'wall/view',
 //				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 //				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-//				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+            ),
+        ),
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
