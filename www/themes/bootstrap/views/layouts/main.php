d40138fce43910a3b058d5e500d3d456157393d6
hunke <hunke@ukr.net>
2014-11-17 11:36:52 +0200

    updated jquery, removed unnecessary files

--- www/themes/bootstrap/views/layouts/main.php
+++ www/themes/bootstrap/views/layouts/main.php
@@ -11,20 +11,10 @@ $cs->registerCssFile($themePath . '/assets/css/bootstrap-theme.css');
 /**
  * JavaScripts
  */
-$cs->registerCoreScript('jquery', CClientScript::POS_END);
-$cs->registerCoreScript('jquery.ui', CClientScript::POS_END);
+$cs->registerCoreScript('jquery-2.1.1.min.js', CClientScript::POS_END);
 $cs->registerScriptFile($themePath . '/assets/js/bootstrap.min.js', CClientScript::POS_END);
 $cs->registerScript('tooltip', "$('[data-toggle=\"tooltip\"]').tooltip();$('[data-toggle=\"popover\"]').tooltip()", CClientScript::POS_READY);
 ?>
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="<?php
-echo Yii::app()->theme->baseUrl . '/assets/js/html5shiv.js';
-?>"></script>
-    <script src="<?php
-echo Yii::app()->theme->baseUrl . '/assets/js/respond.min.js';
-?>"></script>
-    <![endif]-->
 
 <?php
 $this->widget('bootstrap.widgets.BsNavbar', array(
@@ -42,69 +32,6 @@ $this->widget('bootstrap.widgets.BsNavbar', array(
                     'url' => array(
                         'site/index'
                     ),
-                    'items' => array(
-                        BsHtml::menuHeader(BsHtml::icon(BsHtml::GLYPHICON_BOOKMARK), array(
-                            'class' => 'text-center',
-                            'style' => 'color:#99cc32;font-size:32px;'
-                        )),
-                        array(
-                            'label' => 'Home',
-                            'url' => array(
-                                '/site/index'
-                            )
-                        ),
-                        array(
-                            'label' => 'About',
-                            'url' => array(
-                                'site/page',
-                                'view' => 'about'
-                            )
-                        ),
-                        array(
-                            'label' => 'Contact',
-                            'url' => array(
-                                '/site/contact'
-                            )
-                        ),
-                        BsHtml::menuDivider(),
-                        array(
-                            'label' => 'Login',
-                            'url' => array(
-                                '/site/login'
-                            ),
-                            'visible' => Yii::app()->user->isGuest,
-                            'icon' => BsHtml::GLYPHICON_LOG_IN
-                        ),
-                        array(
-                            'label' => 'Logout (' . Yii::app()->user->name . ')',
-                            'url' => array(
-                                '/site/logout'
-                            ),
-                            'visible' => !Yii::app()->user->isGuest
-                        ),
-                        array(
-                            'label' => 'Home',
-                            'url' => array(
-                                '/site/index'
-                            ),
-                            'icon' => BsHtml::GLYPHICON_HOME
-                        ),
-                        array(
-                            'label' => 'About',
-                            'url' => array(
-                                '/site/page',
-                                'view' => 'about'
-                            ),
-                            'icon' => BsHtml::GLYPHICON_PAPERCLIP
-                        ),
-                        array(
-                            'label' => 'Contact',
-                            'url' => array(
-                                '/site/contact'
-                            ),
-                            'icon' => BsHtml::GLYPHICON_FLOPPY_OPEN
-                        )
-                    )
                 )
             )
         ),
@@ -151,8 +78,9 @@ $this->widget('bootstrap.widgets.BsNavbar', array(
     )
 ));
 ?>
-
+<div class="container">
 <?php
 echo $content;
 ?>
+</div>
 
